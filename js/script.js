1c43bdbb321298ec9d764f6d923e0721b35a6188
AngeloTripodi <tripodi.angelo@gmail.com>
2022-12-12 16:32:30 +0100

    Add img form data()

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            text : "Hello VueWorld!"
+            text : "Hello VueWorld!",
+            src : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png"
         }
     }
 }).mount('#app')
