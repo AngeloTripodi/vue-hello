eeccb5881b02571817046e13b8d4e9f5ec8d941c
AngeloTripodi <tripodi.angelo@gmail.com>
2022-12-12 17:35:51 +0100

    More test on Vue

--- js/script.js
+++ js/script.js
@@ -4,7 +4,21 @@ createApp({
     data(){
         return{
             text : "Hello VueWorld!",
-            src : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png"
+            src : "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1184px-Vue.js_Logo_2.svg.png",
+            myClass : " ",
+            myImgClass : "w-25",
+            alt : "Vue Logo",
+        }
+    },
+    methods :{
+        sayHello : function(){
+            console.log("Hello!!!");
+        },
+        calc : function(num1, num2){
+            sum = num1 + num2;
+            console.log(sum)
+            return sum;
+            
         }
     }
 }).mount('#app')
